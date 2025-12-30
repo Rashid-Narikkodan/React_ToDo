459d136be883a7e4553afbeeb216c1594b7b1efe
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2025-12-30 06:45:01 +0530

    Type of Todo created, locale storage created

--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,5 +1,5 @@
-export interface Todo {
-  id: string
-  title: string
-  completed: boolean
+export type Todo={
+  id:string,
+  title:string,
+  completed:boolean
 }
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,12 +1,20 @@
-import type { Todo } from "../types/Todo"
+import type { Todo } from "../types/Todo";
 
-const KEY = "todos"
+const KEY = 'todos'
 
-export const saveTodos = (todos: Todo[]) => {
-  localStorage.setItem(KEY, JSON.stringify(todos))
+export const loadTodos=():Todo[]|string|void=>{
+  try{
+    const todos = localStorage.getItem(KEY)
+    if(todos){
+      return JSON.parse(todos)
+    }else{
+      throw new Error("No items exist in Todo list");
+    }
+  }catch(error:unknown){
+    if(error instanceof Error) return error.message 
+  }
 }
 
-export const loadTodos = (): Todo[] => {
-  const data = localStorage.getItem(KEY)
-  return data ? JSON.parse(data) : []
+export const saveTodos=(todos:Todo[])=>{
+      return localStorage.setItem(KEY,JSON.stringify(todos))
 }

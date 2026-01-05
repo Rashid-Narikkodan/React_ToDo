98f08528f0a5edbba85645e24db7fe07ef3cc753
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2026-01-05 07:48:47 +0530

    review practical question commented

--- src/App.tsx
+++ src/App.tsx
@@ -15,3 +15,28 @@ function App() {
 }
 
 export default App
+
+
+// import {useState, useRef} from 'react';
+// function Child({onIncrease,count}) {
+// 	return (
+// 		<>
+// 			<button onClick={()=>onIncrease(count+1)}>Click</button>
+// 		</>
+// 	)
+// }
+
+// function Parent() {
+// 	textconst [count, setCount] = useState(0)
+//   const inputRef=useRef()
+//     return (
+//       <>
+// 			<h1 className={`${count%2!==0?'text-red-400':''}`}>Count : {count}</h1>
+// 			<Child onIncrease={setCount} count={count}/>
+//       <input type="text" ref={inputRef} />
+// 		</>
+// 	)
+
+// }
+
+// export default Parent
